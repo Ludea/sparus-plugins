eacb7c57048cc6085ae28f2ed0fba3e87e8f4a7d
Ludea <ludovicw35@hotmail.com>
2022-11-20 10:29:38 +0100

    Update index.ts

--- tauri-plugin-min-requirements/webview-src/index.ts
+++ tauri-plugin-min-requirements/webview-src/index.ts
@@ -1,5 +1,61 @@
-import { invoke } from '@tauri-apps/api/tauri'
-
-export async function execute() {
-  await invoke('plugin:test|execute')
+interface GPU {
+  name: string;
+  canrungame: boolean;
 }
+
+interface CPU {
+  frequency: number;
+  core: number;
+  canrungame: boolean;
+}
+
+invoke<GPU>("list_available_gpu")
+      .then((value) => {
+        if (!value.canrungame) {
+          setAlerts((previous: any) => [
+            ...previous,
+            {
+              severity: "warning",
+              message: `The minimum is NVIDIA GTX 970 and you have ${value.name}`,
+            },
+          ]);
+        }
+      })
+      .catch((error) => {
+        setAlerts((previous: any) => [
+          ...previous,
+          { severity: "error", message: error },
+        ]);
+      });
+
+    invoke<CPU>("list_cpu_model")
+      .then((value: any) => {
+        if (!value.canrungame) {
+          if (value.core < 4) {
+            setAlerts((previous: any) => [
+              ...previous,
+              {
+                severity: "warning",
+                message: `The minimum of CPU core is 4 and you have only ${value.core}`,
+              },
+            ]);
+          }
+          if (value.frequency <= 2500) {
+            setAlerts((previous: any) => [
+              ...previous,
+              {
+                severity: "warning",
+                message: `The minimum of CPU frequency is 2.5Ghz and you have only ${
+                  Math.floor(value.frequency / 100) / 10
+                }GHz`,
+              },
+            ]);
+          }
+        }
+      })
+      .catch((error) => {
+        setAlerts((previous: any) => [
+          ...previous,
+          { severity: "error", message: error },
+        ]);
+      });
